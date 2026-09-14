b9a98b4e3f21b53d35d4a4a103be43715e93761f
Milton <9051741+MILKTON@users.noreply.github.com>
2026-09-14 16:22:26 -0600

    Skip HTML return test for GET / endpoint
    Mark the HTML return test as skipped.

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('GET /', () => {
-  it('should return HTML', async () => {
+  xit('should return HTML', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
     expect(res.text).toContain('Bootcamp de GitHub Action en Código Facilito');
